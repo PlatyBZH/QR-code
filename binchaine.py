c25e8bf3f172f5311d66284756aa3d81e1791d00
Arthur Réauté <33656809+Starbonker@users.noreply.github.com>
2017-11-21 10:24:33 +0100

    Update binchaine.py

--- binchaine.py
+++ binchaine.py
@@ -3,8 +3,8 @@ def binchaine(message) :
     “Traduction d’une chaîne de caractères en binaire ASCII”
     messagebin=""
     for i in message:
-        B=str(bin(ord(i)))[2:]  #Converti le caractère i en son code binaire ascii
-        while len(B)<8 :  #Tant qu’il n’y a pas 8 caractères, on rajoute un 0 à gauche
+        B=str(bin(ord(i)))[2:]  #Converti le caractère i en son code binaire ISO-8859-1 
+        while len(B)<8 :  #Tant qu’il n’y a pas 8 caractères, on rajoute un 0 à gauche : on a alors 8 bits de code
             B="0"+B
         messagebin+=B
     return messagebin
